288b0610f5f89b0ea83c5c15c7a66463a38e1b68
Mahdi Mohammadi <jnsm89@yahoo.com>
2026-06-09 12:09:46 +0330

    add order item to domain

--- Store.Domain/Entity/Orders/OrderItem.cs
+++ Store.Domain/Entity/Orders/OrderItem.cs
@@ -1,6 +1,28 @@
-﻿namespace Store.Domain.Entity.Orders;
+﻿using Store.Domain.ValueObjects;
+
+namespace Store.Domain.Entity.Orders;
 
 public class OrderItem
 {
-    
+    public Guid ProductId { get; private set; }
+
+    public int Quantity { get; private set; }
+
+    public Money UnitPrice { get; private set; }
+
+    private OrderItem()
+    {
+    }
+
+    internal OrderItem(
+        Guid productId,
+        int quantity,
+        Money unitPrice)
+    {
+        ProductId = productId;
+
+        Quantity = quantity;
+
+        UnitPrice = unitPrice;
+    }
 }
