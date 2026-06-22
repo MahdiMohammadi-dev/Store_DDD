0b4c5390cc0c43ff9fdfd3a0697c18ed91184414
Mahdi Mohammadi <jnsm89@yahoo.com>
2026-06-22 17:46:45 +0330

    add product name command

---
+++ Store.Application/Products/Commands/ChangeProductNameCommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Store.Domain.Repositories;
+using Store.Domain.ValueObjects;
+
+namespace Store.Application.Products.Commands;
+
+public sealed record ChangeProductNameCommand(Guid ProductId,string ProductName):IRequest<bool>;
+
+
+public sealed class ChangeProductNameCommandHandler:IRequestHandler<ChangeProductNameCommand,bool>
+{
+    private readonly IProductRepository _productRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ChangeProductNameCommandHandler(IUnitOfWork unitOfWork, IProductRepository productRepository)
+    {
+        _unitOfWork = unitOfWork;
+        _productRepository = productRepository;
+    }
+
+    public async Task<bool> Handle(ChangeProductNameCommand request, CancellationToken cancellationToken)
+    {
+        var product = await _productRepository.GetByIdAsync(request.ProductId, cancellationToken);
+
+        if (product == null) return false;
+
+        product.ChangeProductName(new ProductName(request.ProductName));
+
+       await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+       return true;
+    }
+}
---
+++ Store.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Store.Domain.Repositories;
+using Store.Persistence.Context;
+
+namespace Store.Persistence.Repositories;
+
+internal sealed class UnitOfWork : IUnitOfWork
+{
+    private readonly StoreDbContext _dbContext;
+
+    public UnitOfWork(StoreDbContext dbContext) => _dbContext = dbContext;
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default) => await _dbContext.SaveChangesAsync(cancellationToken);
+}
