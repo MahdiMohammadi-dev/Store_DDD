272e16db5d81d008f187c6b01206a7936614557a
Mahdi Mohammadi <jnsm89@yahoo.com>
2026-06-23 16:12:23 +0330

    add specification base class

---
+++ Store.Persistence/Repositories/Specification.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.Persistence.Repositories;
+
+using System.Linq.Expressions;
+
+public abstract class Specification<T>
+{
+    public abstract Expression<Func<T, bool>> ExpressionOfSelectData();
+}
