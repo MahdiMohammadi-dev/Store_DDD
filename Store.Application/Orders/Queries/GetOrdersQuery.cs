f77e19e1fcd903709482af292a37982579f30195
Mahdi Mohammadi <jnsm89@yahoo.com>
2026-06-23 11:08:47 +0330

    order query devloped

--- Store.Application/Orders/Queries/GetOrdersQuery.cs
+++ Store.Application/Orders/Queries/GetOrdersQuery.cs
@@ -1,20 +1,32 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Store.Application.Abstractions;
 using Store.Application.Orders.Dtos;
 using Store.Domain.Repositories;
 
 namespace Store.Application.Orders.Queries;
 
-public sealed record GetOrdersQuery():IRequest<List<OrderDto>>;
+public sealed record GetOrdersQuery() : IRequest<List<OrderDto>>;
 
-
-public class GetOrdersQueryHandler :IRequestHandler<GetOrdersQuery,List<OrderDto>>
+public class GetOrdersQueryHandler : IRequestHandler<GetOrdersQuery, List<OrderDto>>
 {
-    
+    private readonly IStoreDbContext _context;
 
+    public GetOrdersQueryHandler(IStoreDbContext context) => _context = context;
 
     public async Task<List<OrderDto>> Handle(GetOrdersQuery request, CancellationToken cancellationToken)
     {
-   
-       return new();
+        return await _context.Orders
+            .AsNoTracking()
+            .Select(x => new OrderDto(
+                x.Id,
+                x.CustomerId,
+                x.Status.ToString(),
+                x.Items.Sum(i => i.UnitPrice.Amount * i.Quantity),
+                x.Items.Select(a =>
+                    new OrderItemDto(a.ProductId,
+                        a.Quantity,
+                        a.UnitPrice.Amount)).ToList()
+            )).ToListAsync(cancellationToken);
     }
 }
